bd2aa1a3443ca53264b2d843ac4be96db8beedce
kingdomshenuli <107787416+kingdomshenuli@users.noreply.github.com>
2022-06-19 12:39:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94705541966']
+global.premium = ['94705541966']
+global.ownernomer = '94705541966'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
